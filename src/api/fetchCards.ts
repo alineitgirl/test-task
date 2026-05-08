d7ab47fe2cdde3755eca2749cbdd06876296d8c4
alineitgirl <queenalina2005@gmail.com>
2026-05-08 12:35:16 +0300

    fix bugs with linter

--- src/api/fetchCards.ts
+++ src/api/fetchCards.ts
@@ -1,29 +1,25 @@
 import type { Collection, Product } from "../types";
 
-export async function fetchCollectionById(id: number) : Promise<Product[]> {
-    const BASE_URL = import.meta.env.VITE_API_URL;
-    const res = await fetch(BASE_URL);
+export async function fetchCollectionById(id: number): Promise<Product[]> {
+  const BASE_URL = import.meta.env.VITE_API_URL;
+  const res = await fetch(BASE_URL);
 
-    if (!res.ok) {
-        throw new Error(`Ошибка сервера: ${res.status}`);
-    }
+  if (!res.ok) {
+    throw new Error(`Ошибка сервера: ${res.status}`);
+  }
 
-    const data = await res.json();
-    console.log('API Response:', data);
+  const data = await res.json();
 
-    let collections: Collection[] = [];
-    
-    if (data.data && Array.isArray(data.data)) {
-        collections = data.data;
-    } else {
-        throw new Error(`Неожиданный формат ответа API`);
-    }
+  if (!data.data || !Array.isArray(data.data)) {
+    throw new Error(`Неожиданный формат ответа API`);
+  }
 
-    const found = collections.find(c => c.id === id);
+  const collections: Collection[] = data.data;
+  const found = collections.find(c => c.id === id);
 
-    if (!found) {
-        throw new Error(`Коллекция с id ${id} не найдена.`);
-    }
+  if (!found) {
+    throw new Error(`Коллекция с id ${id} не найдена.`);
+  }
 
-    return found.data;
+  return found.data;
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -35,3 +35,4 @@ export interface  CardListProps {
 export interface WidgetProps {
     collectionId?: number;
 }
+
